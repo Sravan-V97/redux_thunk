0913cfe290f9d06a299d3b57c6c3eaa7b546eafc
sravan <myselfsravanjr@gmail.com>
2021-02-27 12:14:58 +0530

    errors to be cleared

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import fetchUsers from "./services/thunk";
+import { useEffect } from "react";
 
 function App() {
   const dispatch = useDispatch();
-  dispatch(fetchUsers());
-  const obtainedUsers = useSelector((state) => state.users);
-  // console.log(obtainedUsers);
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
 
-  return <div className="App"></div>;
+  const obtainedUsers = useSelector((state) => state.users);
+  console.log(obtainedUsers);
+
+  const list = document.getElementById("list");
+
+  window.onload = () => {
+    list.innerHTML = obtainedUsers.map((i) => `<li>${i}</li>`).join("");
+  };
+
+  return (
+    <div className="App">
+      <ul id="list"></ul>;
+    </div>
+  );
 }
 
 export default App;
